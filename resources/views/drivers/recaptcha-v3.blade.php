985b9cb548b7d33161f76068fa522475c2e68070
Daniel Neto <danielneto314@gmail.com>
2026-03-29 12:54:30 -0300

    refactor(views): remove not_configured fallback from driver views and translations

--- resources/views/drivers/recaptcha-v3.blade.php
+++ resources/views/drivers/recaptcha-v3.blade.php
@@ -1,42 +1,32 @@
-@php
-  $siteKey = $getSiteKey();
-@endphp
-
-@if ($siteKey)
-  <div
-    wire:ignore
-    x-data="{
-      response: $wire.$entangle('{{ $getStatePath() }}'),
-      siteKey: '{{ $siteKey }}',
-      init() {
-        if (typeof grecaptcha === 'undefined') {
-          const script = document.createElement('script')
-          script.src = '{{ $getScriptUrl() }}'
-          script.async = true
-          script.defer = true
-          script.onload = () => {
-            grecaptcha.ready(() => {
-              this.executeRecaptcha()
-            })
-          }
-          document.head.appendChild(script)
-        } else {
+<div
+  wire:ignore
+  x-data="{
+    response: $wire.$entangle('{{ $getStatePath() }}'),
+    siteKey: '{{ $getSiteKey() }}',
+    init() {
+      if (typeof grecaptcha === 'undefined') {
+        const script = document.createElement('script')
+        script.src = '{{ $getScriptUrl() }}'
+        script.async = true
+        script.defer = true
+        script.onload = () => {
           grecaptcha.ready(() => {
             this.executeRecaptcha()
           })
         }
-      },
-      executeRecaptcha() {
-        grecaptcha.execute(this.siteKey, { action: 'submit' }).then((token) => {
-          this.response = token
+        document.head.appendChild(script)
+      } else {
+        grecaptcha.ready(() => {
+          this.executeRecaptcha()
         })
-      },
-    }"
-  >
-    <input type="hidden" x-model="response" />
-  </div>
-@else
-  <div class="flex justify-center text-sm text-gray-500">
-    {{ __("filament-captcha::messages.not_configured_recaptcha_v3") }}
-  </div>
-@endif
+      }
+    },
+    executeRecaptcha() {
+      grecaptcha.execute(this.siteKey, { action: 'submit' }).then((token) => {
+        this.response = token
+      })
+    },
+  }"
+>
+  <input type="hidden" x-model="response" />
+</div>
